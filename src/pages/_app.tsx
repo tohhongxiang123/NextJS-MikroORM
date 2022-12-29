ded04f8a47b4c3c3822d5cdfe952c0e8e41ff55f
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-12-29 17:37:31 +0800

    feat: Add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { trpc } from '../utils/trpc';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<SessionProvider session={session}>
 			<Component {...pageProps} />
 		</SessionProvider>
 	)
 }
+
+export default trpc.withTRPC(App);
--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,6 +1,7 @@
 import { GetServerSidePropsContext } from "next";
 import { getSession, useSession } from "next-auth/react";
 import React, { useState } from "react";
+import { trpc } from "../utils/trpc";
 
 export default function DeckPage({ }) {
     const [name, setName] = useState('')
@@ -10,10 +11,17 @@ export default function DeckPage({ }) {
     const handleCreateDeck = (e: React.FormEvent) => {
         e.preventDefault()
     }
+
+    const hello = trpc.hello.useQuery({ text: 'bbb' })
+
+    if (!hello.data) {
+        return <p>Loading...</p>
+    }
     
     return (
         <div>
             <h1>Decks</h1>
+            <p>{hello.data.greeting}</p>
             <form onSubmit={handleCreateDeck}>
                 <input type="text" value={name} onChange={e => setName(e.target.value)} />
                 <input type="text" value={description} onChange={e => setDescription(e.target.value)} />
